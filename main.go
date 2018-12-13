a0973770c8d1b238c00821f62e1a2138ca24c46e
Papik Tamas <trapacska@gmail.com>
2018-12-13 12:05:50 +0100

    initial codesign check

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-tools/go-steputils/stepconf"
+	"github.com/bitrise-tools/go-xcode/certificateutil"
 )
+
+var flutterConfigPath = filepath.Join(os.Getenv("HOME"), ".flutter_settings")
 
 type config struct {
 	IOSAdditionalParams     string `env:"ios_additional_params"`
@@ -27,6 +31,32 @@ func main() {
 		failf("Issue with input: %s", err)
 	}
 	stepconf.Print(cfg)
+
+	if cfg.Platform == "ios" || cfg.Platform == "both" {
+		// exists, err := pathutil.IsPathExists(flutterConfigPath)
+		// if err != nil {
+		// 	failf("Failed to check if path exists, error: %s", err)
+		// }
+
+		// flutterSettingsContent, err := fileutil.ReadBytesFromFile(flutterConfigPath)
+		// if err != nil {
+		// 	failf("Failed to check if path exists, error: %s", err)
+		// }
+
+		// var flutterSettings map[string]string
+		// if err := json.Unmarshal(flutterSettingsContent, flutterSettings); err != nil {
+
+		// }
+
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+		installedCertificates, err := certificateutil.InstalledCodesigningCertificateNames()
+		fmt.Println(installedCertificates, err)
+		fmt.Println()
+		fmt.Println()
+		fmt.Println()
+	}
 
 	for _, spec := range []buildSpecification{
 		buildSpecification{
